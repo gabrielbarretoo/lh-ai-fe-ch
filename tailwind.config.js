6ad6ccd3c6154a9a718d7add2620430f8591b76d
Gabriel Barreto <gabriel.barreto123@gmail.com>
2026-02-01 16:34:50 -0300

    feat: add fonts, tailwind config and deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ui: ['"IBM Plex Sans"', 'ui-sans-serif', 'system-ui'],
+        brief: ['"Source Serif 4"', 'ui-serif', 'Georgia', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
